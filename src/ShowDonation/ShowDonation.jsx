f673de7873b1a54783c00c1ee627d6b7c63fe689
mdzahidhasan53 <mzhasan53@gmail.com>
2023-09-26 18:02:51 -0700

    conditonal set show all button

--- src/ShowDonation/ShowDonation.jsx
+++ src/ShowDonation/ShowDonation.jsx
@@ -1,11 +1,18 @@
 
 
-const ShowDonation = ({data}) => {
-    const {img_rectangle, category_title, title} = data;
+const ShowDonation = ({ data }) => {
+    const { img_rectangle, category_title, title, category_bg_color,font_color,text_bg_color,price } = data;
     return (
-        <div>
-            <img src={img_rectangle} alt="" />
-            <h3 className="text-2xl">{title}</h3>
+        <div style={{background:`${category_bg_color}`}} className="card card-side shadow-xl">
+            <figure><img className="h-full" src={img_rectangle} alt="Movie" /></figure>
+            <div className="card-body">
+                <p style={{background:`${text_bg_color}`, color:`${font_color}`}} className="w-fit p-1 rounded-md">{category_title}</p>
+                <h2 className="card-title">{title}</h2>
+                <p style={{color:`${font_color}`}} className="font-bold">$ {price}</p>
+                <div className="card-actions">
+                    <button style={{background:`${font_color}`}} className="btn normal-case text-white">View Details</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Dnation/Donation.jsx
+++ src/components/Dnation/Donation.jsx
@@ -1,37 +1,46 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import { getStoredDonation} from "../../utility/localstorage";
+import { getStoredDonation } from "../../utility/localstorage";
 import ShowDonation from "../../ShowDonation/ShowDonation";
 
 const Donation = () => {
     const donations = useLoaderData();
     const [donationData, setDonationData] = useState([]);
-    useEffect(()=>{
+    const [dataLength, setDataLength] = useState(4);
+    useEffect(() => {
         const storedDonationIds = getStoredDonation();
-        if(donations.length > 0){
+        if (donations.length > 0) {
             const saveDonation = [];
-            for(const id of storedDonationIds){
+            for (const id of storedDonationIds) {
                 const donation = donations.find(donation => donation.id == id);
-                if(donation){
+                if (donation) {
                     saveDonation.push(donation)
                 }
             }
             setDonationData(saveDonation);
         }
-    },[donations])
+    }, [donations])
 
     return (
-        <div>
-           <h2 className="text-3xl">This is Donation Compononent: {donationData.length}</h2> 
-           <div>
-            {
-                donationData.map(data=><ShowDonation
-                key={data.id}
-                data={data}
-                ></ShowDonation>)
-            }
-           </div>
+        <div className="container mx-auto">
+            <h2 className="text-3xl">This is Donation Compononent: {donationData.length}</h2>
+            <div className="grid grid-cols-2 gap-5">
+                {
+                    donationData.slice(0, dataLength).map(data => <ShowDonation
+                        key={data.id}
+                        data={data}
+                    ></ShowDonation>)
+                }
+            </div>
+            <div className="flex justify-center mt-12">
+                <div className={dataLength === donationData.length && 'hidden'}>
+                    <button
+                        onClick={() => setDataLength(donationData.length)}
+                        className="btn bg-[#009444] text-white hover:bg-orange-600 normal-case">Show All</button>
+                </div>
+            </div>
         </div>
+
     );
 };
 
